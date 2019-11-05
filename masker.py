a7c74662c7400d4bdff8e043dc6655d3c6973b2e
Goutham Kumar Y <gouthamproai@gmail.com>
2019-11-05 13:53:01 +0530

    Added argparse

--- masker.py
+++ masker.py
@@ -5,8 +5,10 @@ from io import StringIO
 from struct import pack
 import base64
 import json
+import argparse
 
 import boto3
+from botocore.exceptions import ClientError
 import pandas as pd
 from Crypto.Hash import MD5
 from Crypto.Cipher import Blowfish
@@ -17,7 +19,6 @@ class Masker():
 	def __init__(self):
 		self.s3 = boto3.client('s3')
 		self.secret_key=self.get_secret()
-		print(self.secret_key)
 		self.json_log={}
 
 	def get_from_s3(self,bucket_name,object_name,local_file_name):
@@ -74,11 +75,13 @@ class Masker():
 	def save_local(self):
 		self.calculate_convertedfile_md5()
 		self.masked_file_name=(self.file_name).split('.')[0]+'_md5_'+self.md5+'.'+(self.file_name).split('.')[1]
-		self.json_log['masked_filename']=self.masked_file_name
+		self.log_filename=self.md5+'.json'
+		self.json_log['masked_file_name']=self.masked_file_name
 		with open(self.masked_file_name, 'w+') as masked_file:
 			masked_file.write(self.converted_fileobj.getvalue())
-		with open(self.md5+'.json','w+') as logfile:
+		with open(self.log_filename,'w+') as logfile:
 			json.dump(self.json_log,logfile)
+		self.json_log['log_filename']=self.log_filename
 		return 0
 
 	def upload_to_s3(self,bucket):
@@ -87,13 +90,32 @@ class Masker():
 			response = s3_client.upload_file(self.masked_file_name, bucket, self.masked_file_name)
 		except ClientError as e:
 			return (False,e)
-		return (True,"Success")
+		try:
+			response = s3_client.upload_file(self.log_filename, 'masked-logstore', self.log_filename)
+		except ClientError as e:
+			return (False,e)
+		print("The files masked file : %s is in bucket masked-filestore \nLogfile: %s is in bucket masked-logstore\n"%(self.masked_file_name,self.log_filename))
+				
 
 if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='CSV Masker')
+	parser.add_argument('-r','--revmask' ,nargs='+',help='Names of columns that are to be Reversibly masked.')
+	parser.add_argument('-ir','--irrevmask',nargs='+',help='Names of columns that are to be Irreversibly masked.')
+	parser.add_argument('-sb','--source_bucket',metavar=('source_bucket', 'prefixed_filename_s3','destination flename'),nargs=3,help='Give bucket name Followed by prefixed objectname and also a filename to be stored as for future usage.')
+	args = parser.parse_args()
+	
 	mask_plz=Masker()
+	mask_plz.get_from_s3(bucket_name=args.source_bucket[0],object_name=args.source_bucket[1],local_file_name=args.source_bucket[2])
+	mask_plz.rev_mask(args.revmask)
+	mask_plz.irrev_mask(args.irrevmask)
+	mask_plz.save_to_object()
+	mask_plz.save_local()
+	mask_plz.upload_to_s3('masked-filestore')
+	"""
 	mask_plz.get_from_s3(bucket_name='unmasked-filestore',object_name='sample.csv',local_file_name='sample.csv')
 	mask_plz.rev_mask(['Birthplace'])
 	mask_plz.irrev_mask(['Code'])
 	mask_plz.save_to_object()
 	mask_plz.save_local()
 	mask_plz.upload_to_s3('masked-filestore')
+	"""
